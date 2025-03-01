70752c895d4195bd7eb48cde8a70caf90ce1cad1
Maria <mariadasgracas318@gmail.com>
2025-02-28 21:42:26 -0300

    jogo versão final

--- campominado.py
+++ campominado.py
@@ -11,7 +11,8 @@
 # possiveis erros: numeros inválidos (str, não esteja em determinado intervalo, float)
 
 from random import randint
-from os import system
+import os
+import time
 
 class Tabuleiro:
     def __init__(self):
@@ -73,17 +74,15 @@ class Bomba(Tabuleiro):
         super().__init__()
 
     def inserirBombas(self, abrirCasa):
-        abrirCasa  # Definir a posição da jogada inicial
+        abrirCasa 
         self.bombas_local = {}
         
-        contador_bombas = 0  # Contador para as bombas
+        contador_bombas = 0
         while contador_bombas < 9:
             l_sorteada = randint(0, self.linhas-1)
             c_sorteada = randint(0, self.colunas-1)
             
-            # Verificar se a casa sorteada não é a casa do jogador nem adjacente à ela
             if (l_sorteada != self.jogada_l or c_sorteada != self.jogada_c) and self.tabuleiro[l_sorteada][c_sorteada] != 'b':
-                # Verificar se a casa sorteada é adjacente à casa do jogador
                 adjacente = False
                 self.casasAdjacentes(self.jogada_l, self.jogada_c)
                 for adj in self.adjacentes:
@@ -91,13 +90,10 @@ class Bomba(Tabuleiro):
                         adjacente = True
                         break
                 
-                # Se a casa sorteada não for adjacente e não for a casa do jogador, podemos colocar a bomba
                 if not adjacente:
                     self.bombas_local[contador_bombas] = {'l': l_sorteada, 'c': c_sorteada}
                     self.tabuleiro[l_sorteada][c_sorteada] = 'b'
-                    contador_bombas += 1  # Incrementar o contador de bombas
-
-
+                    contador_bombas += 1  
 
     def contarBombas(self):
         casas = []
@@ -119,6 +115,7 @@ class Bomba(Tabuleiro):
                         cont += 1
                 else:
                     cont = -1
+
             numeros[str(idx)] = cont
 
         self.numeros = numeros
@@ -134,7 +131,7 @@ class Jogada(Bomba):
         if abrircasa != 0:
             if self.tabuleiro[self.jogada_l][self.jogada_c] == 'b':
                 self.abriuBomba = True
-                return
+            
             abrircasa
             fila_abertura = [(self.jogada_l, self.jogada_c)]  
             casas_abertas = set() 
@@ -158,7 +155,6 @@ class Jogada(Bomba):
                 if self.tabuleiro[self.jogada_l][self.jogada_c] != 'b':
                     self.bombaBandeira = True
                     print('\nNão é possível inserir mais bombas.')
-                    return
                 self.qnt_bandeiras -= 1
                 self.tabuleiro[self.jogada_l][self.jogada_c] = 'X'
             else:
@@ -203,6 +199,8 @@ class Jogada(Bomba):
 class Jogar(Jogada):
     def __init__(self):
         super().__init__()
+        self.tempo_t = 0
+        self.nome = ''
 
     def exibirSimbolos(self):
         lista = ['X', '*', 'b', '1', '0']
@@ -230,34 +228,9 @@ class Jogar(Jogada):
         print()
     
     def exibirTabuleiro(self):
+        os.system('cls')
         self.exibirSimbolos()
         if self.abriuBomba:
-            print('\nO JOGADOR ABRIU UMA BOMBA. VOCÊ PERDEU...\n')
-            print(' ', end='')
-            for c in range(self.colunas):
-                print(f' {c} ', end='')
-            print()
-            for l in range(self.linhas):
-                print(l, end=' ')
-                for c in range(self.colunas):
-                    elemento = self.tabuleiro[l][c]
-                    if elemento == '0':
-                        print('\033[0;37;42m' + '  ', end=' ')
-                    elif elemento in '12345678':
-                        print(f'\033[0;34;47m {elemento}', end=' ')
-                    elif elemento == 'X':
-                        print('\033[0;31;47m' + ' X', end=' ')  
-                    elif elemento == 'b':
-                        print('\033[0;37;41m' + ' *', end=' ')  
-                    else:
-                        print('\033[0;30;0m' + ' *', end=' ')
-                print('\033[0m')
-        else:
-            if self.qnt_bandeiras == 0 and self.bombaBandeira:
-                print('\nO JOGADOR INSERIU A BANDEIRA NO LUGAL ERRADO. VOCÊ PERDEU...\n')
-            elif self.qnt_bandeiras == 0 and not self.bombaBandeira:
-                print('\nO JOGADOR DESCOBRIU TODAS AS BOMBA. VOCÊ GANHOU!\n')
-
             print(' '*2, end='')
             for c in range(self.colunas):
                 print(f' {c} ', end='')
@@ -272,12 +245,38 @@ class Jogar(Jogada):
                         print(f'\033[0;34;47m {elemento}', end=' ')
                     elif elemento == 'X':
                         print('\033[0;31;47m' + ' X', end=' ')  
-                    elif elemento == 'b':
+                    elif elemento == 'b' or elemento == '-1':
+                        print('\033[0;37;41m' + ' *', end=' ')  
+                    else:
+                        print('\033[0;30;0m' + ' *', end=' ')
+                print('\033[0m')
+            print('\nO JOGADOR ABRIU UMA BOMBA. VOCÊ PERDEU...\n')
+        else:
+            print(' '*2, end='')
+            for c in range(self.colunas):
+                print(f' {c} ', end='')
+            print()
+            for l in range(self.linhas):
+                print(l, end=' ')
+                for c in range(self.colunas):
+                    elemento = self.tabuleiro[l][c]
+                    if elemento == '0':
+                        print('\033[0;37;42m' + '  ', end=' ')
+                    elif elemento in '12345678':
+                        print(f'\033[0;34;47m {elemento}', end=' ')
+                    elif elemento == 'X':
+                        print('\033[0;31;47m' + ' X', end=' ')  
+                    elif elemento == 'b' or elemento == '-1':
                         print('\033[0;30;0m' + ' *', end=' ')  
                     else:
                         print('\033[0;30;0m' + ' *', end=' ')
                 print('\033[0m')
             
+            if self.qnt_bandeiras == 0:
+                if self.bombaBandeira:
+                    print('\nO JOGADOR INSERIU A BANDEIRA NO LUGAL ERRADO. VOCÊ PERDEU...\n')
+                else:
+                    print('\nO JOGADOR DESCOBRIU TODAS AS BOMBA. VOCÊ GANHOU!\n')
 
     def menu(self):
         print()
@@ -286,32 +285,64 @@ class Jogar(Jogada):
         print('-='*10)
     
         print('\n1 - Iniciar/Reiniciar\n2 - Abrir Casa\n3 - Inserir Bandeira\n4 - Sair')
-        
+
+    def cadastro(self, nome, tempo):
+        with open('cadastro.txt', 'a', encoding='utf-8') as arq:
+            arq.write(nome + '\n')
+            arq.write(str(tempo) + '\n')
+            arq.close()
+
+        with open('cadastro.txt', 'r', encoding='utf-8') as arq:
+            self.linhas_arq = arq.readlines()
+
+    def exibirTempo(self):
+        print(f'\nTEMPO DE {self.linhas_arq[0][:-1]}: {self.linhas_arq[-1][:-1]} s.')
+
     def opcao(self):
-        system('cls')
+        os.system('cls')
         def verificar_opcao():
             self.menu()
             while True:
                 try:
                     self.opcao = int(input('\nDigite a opção escolhida: ' ))
                     if self.opcao < 1 or self.opcao > 4: 
-                        system('cls')
+                        os.system('cls')
                         self.menu()
                         print('\nOpção inválida. Tente novamente.')
                         continue
                 except ValueError:
-                    system('cls')
-                    self.menu()
                     print('\nDigite somente números inteiros.')
                 else:
                     break
+
         jogar = False
+        finalizar = False
         while True:
             verificar_opcao()
+            if self.qnt_bandeiras == 0 or self.abriuBomba:
+                finalizar = True
+                self.fim = time.time()
+                self.tempo_t = int(self.fim - self.inicio)
+                self.cadastro(self.nome, self.tempo_t)
+                if self.opcao == 2 or self.opcao == 3:
+                    os.system('cls')
+                    print('\nNão é possível fazer mais jogadas. Inicie outra partida.')
             if self.opcao == 4:
+                if os.path.exists('cadastro.txt'):
+                    os.remove('cadastro.txt')
+                self.fim = time.time()
+                self.tempo_t = int(self.fim - self.inicio)
+                self.cadastro(self.nome, self.tempo_t)
+                self.exibirTempo()
                 break
-            elif self.opcao == 1:
-                system('cls')
+            if self.opcao == 1:
+                self.tempo_t = 0
+                finalizar = False
+                self.inicio = time.time()          
+                self.abriuBomba = False
+                self.bombaBandeira = False
+                self.qnt_bandeiras = 9
+                os.system('cls')
                 print('  ', end='')
                 for coluna in range(self.colunas):
                     print(f' {coluna} ', end='')
@@ -323,38 +354,42 @@ class Jogar(Jogada):
                     print()
                 if jogar:
                     self.tabuleiro = []
-                    self.abriuBomba = False
-                    self.bombaBandeira = False
-                    self.qnt_bandeiras = 9
                     abrir_casa = self.abrirCasa()
                     self.gerarTabuleiro(abrir_casa)
                     self.revelarCasas(abrir_casa, 0)
-                    system('cls')
+                    os.system('cls')
                     self.exibirTabuleiro()
                 else:
                     jogar = True
+                    self.nome = input('\nDigite seu nome: ').upper().strip()
+                    while self.nome == '':
+                        self.nome = input('\nDigite seu nome: ').upper().strip()
                     abrir_casa = self.abrirCasa()
                     self.gerarTabuleiro(abrir_casa)
                     self.revelarCasas(abrir_casa, 0)
-                    system('cls')
+                    os.system('cls')
                     self.exibirTabuleiro()
-            elif self.opcao == 2:
-                if jogar:
-                    abrir_casa = self.abrirCasa()
-                    self.revelarCasas(abrir_casa, 0)
-                    system('cls')
-                    self.exibirTabuleiro()
-                else:
-                    print('Opção inválida. Digite 1 para iniciar o jogo.')
-            else:
-                if jogar:
-                    colocar_bandeira = self.inserirBandeira()
-                    self.revelarCasas(0, colocar_bandeira)
-                    system('cls')
-                    self.exibirTabuleiro()
-                else:
-                    print('Opção inválida. Digite 1 para iniciar o jogo.')
+            if not finalizar:
+                if self.opcao == 2:
+                    if jogar:
+                        abrir_casa = self.abrirCasa()
+                        self.revelarCasas(abrir_casa, 0)
+                        os.system('cls')
+                        self.exibirTabuleiro()
+                    else:
+                        os.system('cls')
+                        print('\nOpção inválida. Digite 1 para iniciar o jogo.')
+                elif self.opcao == 3:   
+                    if jogar:
+                        colocar_bandeira = self.inserirBandeira()
+                        self.revelarCasas(0, colocar_bandeira)
+                        os.system('cls')
+                        self.exibirTabuleiro()
+                    else:
+                        os.system('cls')
+                        print('\nOpção inválida. Digite 1 para iniciar o jogo.')
 
-
-tb = Jogar()
-tb.opcao()
+if __name__ == '__main__':
+    tb = Jogar()
+    tb.opcao()
+    
